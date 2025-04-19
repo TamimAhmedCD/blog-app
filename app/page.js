e31f2e2d452e26b97ed8be7456226677843d7a76
Tamim Ahmed <tamim20072@gmail.com>
2025-04-19 16:46:40 +0600

    All blog added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+import BlogItem from "@/components/BlogItem";
+import BlogList from "@/components/BlogList";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <BlogList />
     </>
   );
 }
